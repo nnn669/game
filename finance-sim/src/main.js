575328a6f4477208bad54f0990ef5e77b8594e9a
nnn669 <510875510@qq.com>
2026-03-17 00:39:04 +0800

    完整上传 finance-sim 项目

--- finance-sim/src/main.js
+++ finance-sim/src/main.js
@@ -1,5 +1,3 @@
-import 'core-js/features/array/at'
-import 'core-js/features/array/at'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
@@ -8,11 +6,3 @@ import './styles/global.css'
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
-npm run build
-cp -r dist/* ../
-cd ..
-git add .
-git commit -m "添加兼容性补丁"
-git push
-
-
