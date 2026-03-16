9b7d6f1e3a25695281feaaa4d2f534996da738c6
nnn669 <510875510@qq.com>
2026-03-16 21:45:04 +0800

    添加兼容性补丁

--- finance-sim/src/main.js
+++ finance-sim/src/main.js
@@ -1,3 +1,4 @@
+import 'core-js/features/array/at'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
@@ -6,3 +7,11 @@ import './styles/global.css'
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
+npm run build
+cp -r dist/* ../
+cd ..
+git add .
+git commit -m "添加兼容性补丁"
+git push
+
+
